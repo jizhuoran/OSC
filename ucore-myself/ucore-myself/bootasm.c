3e4da1c704cdca1cc66e53be9a1877b437f92910
jizhuoran <jizhuoran@jizhuorandeMacBook-Pro.local>
2016-07-05 16:12:28 +0800

    ucore讲解开课啦，😂

---
+++ ucore-myself/ucore-myself/bootasm.c
@@ -0,0 +1,9 @@
+//
+//  bootasm.c
+//  ucore-myself
+//
+//  Created by jizhuoran on 16/7/5.
+//  Copyright © 2016年 jizhuoran. All rights reserved.
+//
+
+#include "bootasm.h"
---
+++ ucore-myself/ucore-myself/bootasm.h
@@ -0,0 +1,14 @@
+//
+//  bootasm.h
+//  ucore-myself
+//
+//  Created by jizhuoran on 16/7/5.
+//  Copyright © 2016年 jizhuoran. All rights reserved.
+//
+
+#ifndef bootasm_h
+#define bootasm_h
+
+#include <stdio.h>
+
+#endif /* bootasm_h */
